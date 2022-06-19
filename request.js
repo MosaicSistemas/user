66247c94be8576cdf0ef6e022930d62d23eb89c9
MosaicBridge <igorcrissaff06@gmail.com>
2022-06-19 13:31:39 -0300

    login particles

--- request.js
+++ request.js
@@ -2,6 +2,11 @@ function request(method, url, body) {
     var request = new XMLHttpRequest()
     request.open(method, url, true)
     request.setRequestHeader('Content-type', 'application/json; charset=UTF-8')
+    request.onload = function () {
+        // Begin accessing JSON data here
+        var data = JSON.parse(this.response)
+
+      }
     if (body) {
         request.send(body)
     }
